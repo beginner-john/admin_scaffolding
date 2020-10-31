64ab15296dfffe0af43b295b94c4dfad6c2cc479
bo.xiong <bo.xiong@transsion.com>
2020-10-31 14:29:22 +0800

    init

---
+++ src/main/java/com/generic/admin_scaffolding/AdminScaffoldingApplication.java
@@ -0,0 +1,13 @@
+package com.generic.admin_scaffolding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminScaffoldingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminScaffoldingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/generic/admin_scaffolding/AdminScaffoldingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generic.admin_scaffolding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminScaffoldingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
